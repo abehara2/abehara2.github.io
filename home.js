a2fb3ad87e268c089bcbc81adba1c398bf4f7888
ashankbehara <abehara2@illinois.edu>
2019-11-18 07:30:51 -0600

    Huge changes

---
+++ home.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// Typing Animation
+	new TypeIt('#whatIAm', {
+		strings: [' Ashank Behara.',' an athlete.', ' an aspiring engineer.', ' a musician.', ' not a squid.', ' motivated.' , ' Ashank Behara.'],
+		breakLines: false,
+		nextStringDelay: 2000,
+		startDelay: 550
+	});
+});
